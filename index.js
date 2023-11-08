76f7630c59a6d9d9def45ccc0c7ea008f249a974
argonaultes <gael@argonaultes.fr>
2023-11-08 21:14:15 +0100

    random cpu load on each request

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const os = require('os');
 
 const networkInterfaces = os.networkInterfaces();
 
-const server = Bun.serve({
+ips = []
+
+for (const iface in networkInterfaces) {
+    for (const addr of networkInterfaces[iface]) {
+        if (addr.family == 'IPv4') {
+            ips.push(addr.address)
+        }
+    }
+}
+
+Bun.serve({
     port: 3002,
     fetch(req) {
-        return new Response(`it works from ${JSON.stringify(networkInterfaces)} ! ${process.env.API_TOKEN}`);
+        const start = Date.now();
+        const max = (Number.MAX_SAFE_INTEGER / 1000000) * Math.random();
+        for (let i = 0 ; i < max ; i++) {}
+        const end = Date.now();
+        console.log(`${new Date(Date.now()).toLocaleString()}:\t${ips.join('\t')}\t1 request\t${(end - start) / 1000} seconds\t${max}`);
+        return new Response(`it works from ${ips.join(', ')} ! ${process.env.API_TOKEN}`);
     },
 })
