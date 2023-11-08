36d9e77d81a79e1f27f8cdeb93c21736eeb7156f
argonaultes <gael@argonaultes.fr>
2023-11-08 17:30:05 +0100

    read from all networkinterfaces, should one be the one

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const os = require('os');
+
+const networkInterfaces = os.networkInterfaces();
+
 const server = Bun.serve({
     port: 3002,
     fetch(req) {
-        return new Response(`it works from ${JSON.stringify(server.requestIP(req))} ! ${process.env.API_TOKEN}`);
+        return new Response(`it works from ${JSON.stringify(networkInterfaces)} ! ${process.env.API_TOKEN}`);
     },
 })
