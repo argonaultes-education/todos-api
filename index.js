74ebc2f4e761f00dc27c1b79edd7ee04ee67d770
argonaultes <gael@argonaultes.fr>
2023-11-08 17:17:07 +0100

    returns the IP of the requestIP

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = Bun.serve({
     port: 3002,
     fetch(req) {
-        return new Response('it works !');
+        return new Response(`it works from ${JSON.stringify(server.requestIP(req))} ! ${process.env.API_TOKEN}`);
     },
 })
